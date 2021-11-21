5c93d9c4c1717be1402b383d750842b6df9736ee
Josip Ivancic <ivancic.josip@nsoft.com>
2021-11-21 13:40:21 +0100

    Remove boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Welcome from './components/Welcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <Welcome />
-  </main>
+  <div>Hello</div>
 </template>
 
 <style>

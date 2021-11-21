e9cfc29c5460f7c190433edf9e6dcb7d21421dba
Josip Ivancic <ivancic.josip@nsoft.com>
2021-11-21 13:43:56 +0100

    Add dummy component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MyButton from './components/MyButton.vue';
+</script>
 
 <template>
-  <div>Hello</div>
+  <div><MyButton>Click me</MyButton></div>
 </template>
 
 <style>
@@ -13,56 +15,5 @@
   padding: 2rem;
 
   font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 }
 </style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+interface Props {
+  primary?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  primary: false,
+});
+
+const { primary } = reactive(props);
+</script>
+
+<template>
+  <button class="btn" :class="{ primary }">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 0.5rem 1rem;
+}
+.btn.primary {
+  background: hsl(239, 100%, 27%);
+  color: #fff;
+}
+</style>
